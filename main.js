60e997488d74d16bca04f2a5e631d224a568e9a6
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-01 11:54:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-
+//Bot para WhatsApp creado por Samu... Si vas a modificar no quites creditos, GRACIAS!!
+let { WAConnection: _WAConnection, WA_MESSAGE_STUB_TYPES } = require('@adiwajshing/baileys')
+let { generate } = require('qrcode-terminal')
+let qrcode = require('qrcode')
+let simple = require('./lib/simple')
+let logs = require('./lib/logs')
+let yargs = require('yargs/yargs')
+let syntaxerror = require('syntax-error')
+let fetch = require('node-fetch')
+let chalk = require('chalk')
+let fs = require('fs')
+let path = require('path')
+let util = require('util')
+let { spawnSync } = require('child_process')
+let WAConnection = simple.WAConnection(_WAConnection)
